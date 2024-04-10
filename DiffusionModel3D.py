2a213f9eec0f44ff672f426b0a5016160b9db0ae
SBodapati11 <56486664+SBodapati11@users.noreply.github.com>
2024-04-10 10:00:28 -0500

    Update DiffusionModel3D.py

--- DiffusionModel3D.py
+++ DiffusionModel3D.py
@@ -5,7 +5,6 @@ class DiffusionModel3D():
 
     def __init__(self, prompt):
         self.prompt = prompt
-        self.generateTriangleMesh(self.prompt)
 
     def generateTriangleMesh(self):
         pipe = ShapEPipeline.from_pretrained("openai/shap-e").to("cuda")
